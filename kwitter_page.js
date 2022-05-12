7a20293bb9575abd8a37ed051613687b6ab7b44c
Pratik Nayak <104261131+pratikn2008@users.noreply.github.com>
2022-05-12 18:33:16 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -44,7 +44,7 @@ const firebaseConfig = {
              like = message_data['like'];
              name_with_tag = "<h4> "+ name +"<img class='user_tick' src='tick.png'></h4>";
              message_with_tag = "<h4 class='message_h4'>" + message + "</h4>";
-    like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
+             like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='updateLike(this.id)'>";
              span_with_tag = "<span class='glyphicon glyphicon-thumbs-up'>Like: "+ like +"</span></button><hr>";
     
             row = name_with_tag + message_with_tag +like_button + span_with_tag;       
